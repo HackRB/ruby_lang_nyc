ebbba7bb9c2109e7705de4b5c311689e11a7dd7b
dgonzdev <dgonzdev837@gmail.com>
2026-06-24 22:39:04 -0400

    implement frame and cleanup home page events card

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,12 @@
 class EventsController < ApplicationController
   def index
+    @events = {
+      date: Time.parse("2026-7-15"),
+      category: "Talk",
+      title: "Concurrency in Ruby 3.3 — Ractors, Fibers & Beyond",
+      location: "ThoughtWorks NYC, Midtown"
+    }
+
     render "index"
   end
 end
