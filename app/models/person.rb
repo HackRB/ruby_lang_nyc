6fa8ca2d6e3b1b5011da5fc7b408c98ae264c312
dgonzdev <dgonzdev837@gmail.com>
2026-06-27 19:08:05 -0400

    rename website url to blog url

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,48 +6,48 @@ class Person
         role: "Staff Engineer",
         company: "Stripe",
         avatar_url: "https://ui-avatars.com/api/?name=Avital+Oliver&background=e8002d&color=fff&size=128",
-        github_url: "https://github.com/avitaln",
-        website_url: nil
+        blog_url: nil,
+        github_url: "https://github.com/avitaln"
       },
       {
         name: "Jeremy Evans",
         role: "Open Source Developer",
         company: "Sequel / Roda",
         avatar_url: "https://ui-avatars.com/api/?name=Jeremy+Evans&background=1a1a2e&color=fff&size=128",
-        github_url: "https://github.com/jeremyevans",
-        website_url: "https://code.jeremyevans.net"
+        blog_url: "https://code.jeremyevans.net",
+        github_url: "https://github.com/jeremyevans"
       },
       {
         name: "Nadia Odunayo",
         role: "Founder",
         company: "The StoryGraph",
         avatar_url: "https://ui-avatars.com/api/?name=Nadia+Odunayo&background=e8002d&color=fff&size=128",
-        github_url: "https://github.com/nodunayo",
-        website_url: nil
+        blog_url: nil,
+        github_url: "https://github.com/nodunayo"
       },
       {
         name: "Sam Phippen",
         role: "Senior Engineer",
         company: "Honeybadger",
         avatar_url: "https://ui-avatars.com/api/?name=Sam+Phippen&background=1a1a2e&color=fff&size=128",
-        github_url: "https://github.com/samphippen",
-        website_url: nil
+        blog_url: nil,
+        github_url: "https://github.com/samphippen"
       },
       {
         name: "Eileen Uchitelle",
         role: "Principal Engineer",
         company: "Shopify",
         avatar_url: "https://ui-avatars.com/api/?name=Eileen+Uchitelle&background=e8002d&color=fff&size=128",
-        github_url: "https://github.com/eileencodes",
-        website_url: nil
+        blog_url: nil,
+        github_url: "https://github.com/eileencodes"
       },
       {
         name: "Daniel Colson",
         role: "Senior Engineer",
         company: "GitHub",
         avatar_url: "https://ui-avatars.com/api/?name=Daniel+Colson&background=1a1a2e&color=fff&size=128",
-        github_url: "https://github.com/composerinteralia",
-        website_url: nil
+        blog_url: nil,
+        github_url: "https://github.com/composerinteralia"
       }
     ]
   end
