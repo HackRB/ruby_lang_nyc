bf824a1d1a7fdff9419e67f75cde010594a509a5
dgonzdev <dgonzdev837@gmail.com>
2026-06-27 19:15:44 -0400

    rename role to description

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,42 +3,42 @@ class Person
     [
       {
         name: "Avital Oliver",
-        role: "Staff Engineer",
+        description: "Staff Engineer",
         avatar_url: "https://ui-avatars.com/api/?name=Avital+Oliver&background=e8002d&color=fff&size=128",
         blog_url: nil,
         github_url: "https://github.com/avitaln"
       },
       {
         name: "Jeremy Evans",
-        role: "Open Source Developer",
+        description: "Open Source Developer",
         avatar_url: "https://ui-avatars.com/api/?name=Jeremy+Evans&background=1a1a2e&color=fff&size=128",
         blog_url: "https://code.jeremyevans.net",
         github_url: "https://github.com/jeremyevans"
       },
       {
         name: "Nadia Odunayo",
-        role: "Founder",
+        description: "Founder",
         avatar_url: "https://ui-avatars.com/api/?name=Nadia+Odunayo&background=e8002d&color=fff&size=128",
         blog_url: nil,
         github_url: "https://github.com/nodunayo"
       },
       {
         name: "Sam Phippen",
-        role: "Senior Engineer",
+        description: "Senior Engineer",
         avatar_url: "https://ui-avatars.com/api/?name=Sam+Phippen&background=1a1a2e&color=fff&size=128",
         blog_url: nil,
         github_url: "https://github.com/samphippen"
       },
       {
         name: "Eileen Uchitelle",
-        role: "Principal Engineer",
+        description: "Principal Engineer",
         avatar_url: "https://ui-avatars.com/api/?name=Eileen+Uchitelle&background=e8002d&color=fff&size=128",
         blog_url: nil,
         github_url: "https://github.com/eileencodes"
       },
       {
         name: "Daniel Colson",
-        role: "Senior Engineer",
+        description: "Senior Engineer",
         avatar_url: "https://ui-avatars.com/api/?name=Daniel+Colson&background=1a1a2e&color=fff&size=128",
         blog_url: nil,
         github_url: "https://github.com/composerinteralia"
