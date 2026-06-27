d7b29614badb2443b7e7da3bbfae4006e31ec706
dgonzdev <dgonzdev837@gmail.com>
2026-06-27 19:14:06 -0400

    remove company

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,7 +4,6 @@ class Person
       {
         name: "Avital Oliver",
         role: "Staff Engineer",
-        company: "Stripe",
         avatar_url: "https://ui-avatars.com/api/?name=Avital+Oliver&background=e8002d&color=fff&size=128",
         blog_url: nil,
         github_url: "https://github.com/avitaln"
@@ -12,7 +11,6 @@ class Person
       {
         name: "Jeremy Evans",
         role: "Open Source Developer",
-        company: "Sequel / Roda",
         avatar_url: "https://ui-avatars.com/api/?name=Jeremy+Evans&background=1a1a2e&color=fff&size=128",
         blog_url: "https://code.jeremyevans.net",
         github_url: "https://github.com/jeremyevans"
@@ -20,7 +18,6 @@ class Person
       {
         name: "Nadia Odunayo",
         role: "Founder",
-        company: "The StoryGraph",
         avatar_url: "https://ui-avatars.com/api/?name=Nadia+Odunayo&background=e8002d&color=fff&size=128",
         blog_url: nil,
         github_url: "https://github.com/nodunayo"
@@ -28,7 +25,6 @@ class Person
       {
         name: "Sam Phippen",
         role: "Senior Engineer",
-        company: "Honeybadger",
         avatar_url: "https://ui-avatars.com/api/?name=Sam+Phippen&background=1a1a2e&color=fff&size=128",
         blog_url: nil,
         github_url: "https://github.com/samphippen"
@@ -36,7 +32,6 @@ class Person
       {
         name: "Eileen Uchitelle",
         role: "Principal Engineer",
-        company: "Shopify",
         avatar_url: "https://ui-avatars.com/api/?name=Eileen+Uchitelle&background=e8002d&color=fff&size=128",
         blog_url: nil,
         github_url: "https://github.com/eileencodes"
@@ -44,7 +39,6 @@ class Person
       {
         name: "Daniel Colson",
         role: "Senior Engineer",
-        company: "GitHub",
         avatar_url: "https://ui-avatars.com/api/?name=Daniel+Colson&background=1a1a2e&color=fff&size=128",
         blog_url: nil,
         github_url: "https://github.com/composerinteralia"
